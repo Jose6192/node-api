9de55813005c16a26e7a3cf9e823e1ddf5bb5b40
Jose6192 <127167425+Jose6192@users.noreply.github.com>
2023-10-21 21:20:22 -0600

    add encrypt passwords

--- back/src/config/init.js
+++ back/src/config/init.js
@@ -1,16 +1,23 @@
 const User = require('../models/user');
+const bcrypt = require('bcryptjs');
+const saltRounds = 10;
+const genericPassword = 'password';
 
 async function createAdminUser() {
   try {
     const admin = await User.findOne({ rol: 'Admin' });
-
     if (!admin) {
-      await User.create({
-        name: 'admin',
-        password: 'password',
-        rol: 'Admin',
-      });
-      console.log('Usuario Admin creado con éxito');
+      bcrypt.hash(genericPassword, saltRounds, async (err, hash) => {
+        if (err) console.error('Error al encriptar la contraseña del admin:', err);
+        else {
+          await User.create({
+          name: 'admin',
+          password: hash,
+          rol: 'Admin',
+          });
+          console.log('Usuario Admin creado con éxito');
+        }
+      })
     }
   } catch (error) {
     console.error('Error al crear el usuario Admin:', error);
--- back/src/routes/user.js
+++ back/src/routes/user.js
@@ -5,17 +5,26 @@ const User = require('../models/user');
 
 const jwt = require('jsonwebtoken');
 
+const bcrypt = require('bcryptjs')
+const saltRounds = 10;
+
 router.get('/', (req, res) => res.send('hello :D'))
 
 router.post('/users/signup', verifyToken, async (req, res) => {
     if (req.UserRol !== 'Admin') return res.status(401).json({ message: 'No tienes permiso para crear usuarios' });
     try {
-        const { name, password, rol } = req.body;
+        let { name, password, rol } = req.body;
         if (await User.findOne({ name })) return res.status(404).json({ message: 'Este usuario ya está registrado' });
-        const newUser = new User({ name, password, rol });
-        await newUser.save();
-        const token = jwt.sign({ _id: newUser._id, role: newUser.rol }, 'secretKey');
-        res.status(200).json({ message: 'Usuario registrado con exito' });
+        bcrypt.hash(password, saltRounds, async (err, hash) => { //encriptacionde la contrasena
+            if (err) res.status(500).json({message: 'Error interno al guardar contraseña'});
+            else {
+                password = hash;
+                const newUser = new User({ name, password, rol });
+                await newUser.save();
+                const token = jwt.sign({ _id: newUser._id, role: newUser.rol }, 'secretKey');
+                res.status(200).json({ message: 'Usuario registrado con exito' });
+            }
+        })
     } catch (error) {
         res.status(500).json({ message: 'Error al registrar usuario' });
     }
@@ -23,15 +32,19 @@ router.post('/users/signup', verifyToken, async (req, res) => {
 
 router.post('/users/signin', async (req, res ) => {
     try {
-        const {name, password} = req.body;
+        let {name, password} = req.body;
         const user = await User.findOne({name});
         if (!user) return res.status(404).json({ message: '¡Oh no! Parece que aun no estas registrado' });
-        if (user.password !== password) return res.status(401).json({ message: '¡Oops! Parece que olvidaste tu contraseña' });
-    
-        const token = jwt.sign({_id: user._id, role: user.rol}, 'secretKey');
-        return res.status(200).json({token});
+        bcrypt.compare(password, user.password, (err, result) => { //comparar contraseña encriptada
+            if (err) res.status(500).json('error interno al leer contraseña')
+            else if (result) {
+                const token = jwt.sign({_id: user._id, role: user.rol}, 'secretKey');
+                return res.status(200).json({token});
+            }
+            else return res.status(401).json({ message: '¡Oops! Parece que olvidaste tu contraseña' });       
+        })
     } catch (error) {
-        res.status(500).json({ message: 'Error al iniciar sesion' });
+        res.status(500).json({ message: 'Error interno al iniciar sesion' });
     }
 });
 
