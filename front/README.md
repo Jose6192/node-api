210dcedf1dad3a3e1fc165cdc02ac8f93b4a1d80
Jose6192 <127167425+Jose6192@users.noreply.github.com>
2024-05-03 21:07:38 -0500

    style: :art: agrega nuevo barra de navegacion

--- front/README.md
+++ front/README.md
@@ -1,27 +1,21 @@
-# Front
+# SISTEMA DE GESTION DE INFRAESTRUCTURA
+¡Aplicacion en desarollo!
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 16.0.3.
+## Requisitos del Sistema
+- Angular
+- MongoDB
 
-## Development server
 
-Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The application will automatically reload if you change any of the source files.
+## Instalacion
+- utilizar el comando `npm install` dentro de la carpeta front para instalar las dependencias
+- utilizar el comando `npm install` dentro de la carpeta back para instalar las dependencias
 
-## Code scaffolding
+## iniciar
+- Inicar el servicio de mongoDB
+- utilizar el comando npm run dev dentro de la carpeta back para iniciar el servidor
+- utilizar el comando ng serve dentro de la carpeta front para inicar el servidor de Angular
+- entrar en el localhost de angular en tu navegador (http://localhost:4200/)
+- el usuario del administrador al inicar el proyecto es usuario: admin contraseña: Admin
 
-Run `ng generate component component-name` to generate a new component. You can also use `ng generate directive|pipe|service|class|guard|interface|enum|module`.
-
-## Build
-
-Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory.
-
-## Running unit tests
-
-Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
-
-## Running end-to-end tests
-
-Run `ng e2e` to execute the end-to-end tests via a platform of your choice. To use this command, you need to first add a package that implements end-to-end testing capabilities.
-
-## Further help
-
-To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
+## Autor
+Este proyecto fue creado por Jose Angel para mas informacion del proyecto mira la memoria "Sistema de gestión de infraestructura" generacion 2023.
--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -6,14 +6,65 @@ import { AuthService } from './services/auth.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent{
-  constructor( public authService: AuthService ) { }
+export class AppComponent {
 
+  isDarkTheme?: boolean;
   sideNavStatus: boolean = false;
+  navCollapsed: boolean = false;
 
-  toggleSideNav() {
-    this.sideNavStatus = !this.sideNavStatus;
+  constructor(public authService: AuthService, ) { 
+    
   }
+
+  ngOnInit(){ //introde en list los elementos que se mostraran en el side-nav dependiendo del rol del usuario
+    if(this.authService.getDataUser().role == 'Admin'){
+      console.log('admin');
+      for(let i = 0; i < this.admin.length; i++){
+        this.list.push(this.admin[i]);
+      }
+    }
+    if(this.authService.getDataUser().role == 'Sistemas'){
+      for(let i = 0; i < this.sistemas.length; i++){
+        this.list.push(this.sistemas[i]);
+      }
+    }
+    //aplica el tema 
+    this.isDarkTheme = localStorage.getItem('theme') === 'dark';
+    this.applyTheme();
+  }
+
+  list = [
+    { name: 'Inicio', icon: 'bi bi-house-door-fill', route: '/tasks' },
+    { name: 'reporte', icon: 'bi bi-clipboard-data-fill', route: '/report-form' },
+    { name: 'historial', icon: 'bi bi-archive-fill', route: '/task-history' },
+    { name: 'Conocimiento', icon: 'bi bi-book-fill', route: '/knowledge'}
+  ]
+
+  admin = [
+    { name: 'Usuarios', icon: 'bi bi-people-fill', route: '/signup' },
+    { name: 'Ficha tecnica', icon: 'bi bi-pc', route: '/technical-form' }
+  ]
+
+  sistemas = [
+    { name: 'Ficha tecnica', icon: 'bi bi-file-earmark-text-fill', route: '/technical-form' }
+  ]
+
+  logOut() {
+    this.authService.logOut();
+  }
+  toggleTheme() {
+    this.isDarkTheme = !this.isDarkTheme;
+    localStorage.setItem('theme', this.isDarkTheme ? 'dark' : 'light');
+    this.applyTheme();
+  }
+  applyTheme() {
+    const theme = this.isDarkTheme ? 'dark' : 'light';
+    const body = document.body as HTMLElement;
+    body.setAttribute('data-bs-theme', theme);
+  }
+  toggleNav() {
+    this.navCollapsed = !this.navCollapsed;
+  };
 
   tokenIsExpired() {
     return this.authService.tokenIsExpired();
