6678a804469c199f3178f955581409567e61ab41
Jose6192 <127167425+Jose6192@users.noreply.github.com>
2024-05-06 11:09:46 -0500

    feat: :sparkles: agrega estadisticas

--- front/src/app/components/statistics/statistics.component.ts
+++ front/src/app/components/statistics/statistics.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Chart, ChartType } from 'chart.js/auto';
 
 @Component({
   selector: 'app-statistics',
   templateUrl: './statistics.component.html',
   styleUrls: ['./statistics.component.css']
 })
-export class StatisticsComponent {
+export class StatisticsComponent implements OnInit {
+
+  chart?: Chart;
+
+  constructor() { }
+
+  data = {
+    labels: ['Edificio C', 'Edificio D', 'Edificio E', 'Edificio F', 'Edificio L', 'Cafeteria', 'Domo'],
+    datasets: [{
+      label: 'Reportes por edificio',
+      data: [10, 19, 20, 11, 16, 20, 1],
+      backgroundColor: 'rgba(54, 162, 235, 0.2)',
+      borderColor: 'rgb(54, 162, 235)',
+      borderWidth: 1
+    }]
+  };
+
+  ngOnInit(): void {
+    this.chart = new Chart('building', {
+      type: 'bar',
+      data: this.data
+    })
+  }
 
 }
